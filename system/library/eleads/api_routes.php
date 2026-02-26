ee8ba416f684304976597bd6c970fda5f0e380d8
Dmitry Plohotnyuk <dmitryplohotnyuk@MacBook-Pro-Dmitry.local>
2026-02-26 11:53:20 +0200

    v0.1.30

--- system/library/eleads/api_routes.php
+++ system/library/eleads/api_routes.php
@@ -1,11 +1,11 @@
 <?php
 class EleadsApiRoutes {
-	const API_BASE = 'https://stage-dashboard.e-leads.net/api/';
-	const WIDGETS_LOADER_TAG = 'https://stage-api.e-leads.net/v1/widgets-loader-tag';
-	const TOKEN_STATUS = 'https://stage-dashboard.e-leads.net/api/ecommerce/token/status';
-	const SEO_SLUGS = 'https://stage-dashboard.e-leads.net/api/seo/slugs';
-	const SEO_PAGE = 'https://stage-dashboard.e-leads.net/api/seo/pages/';
-	const SEARCH_FILTERS = 'https://stage-processing.e-leads.net/api/ecommerce/search/filters';
+	const API_BASE = 'https://dashboard.e-leads.net/api/';
+	const WIDGETS_LOADER_TAG = 'https://api.e-leads.net/v1/widgets-loader-tag';
+	const TOKEN_STATUS = 'https://dashboard.e-leads.net/api/ecommerce/token/status';
+	const SEO_SLUGS = 'https://dashboard.e-leads.net/api/seo/slugs';
+	const SEO_PAGE = 'https://dashboard.e-leads.net/api/seo/pages/';
+	const SEARCH_FILTERS = 'https://processing.e-leads.net/api/ecommerce/search/filters';
 
 	const GITHUB_REPO = 'https://github.com/E-Leads-net/eleads-opencart-4';
 	const GITHUB_LATEST_RELEASE = 'https://api.github.com/repos/E-Leads-net/eleads-opencart-4/releases/latest';
